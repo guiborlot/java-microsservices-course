62e4dc3bd8254986fbd2cd31679bcac2979db23d
Guilherme Borlot <guiborlot@gmail.com>
2022-06-01 19:59:09 -0300

    section 14

---
+++ bank-account/account-query/src/main/java/com/techbank/account/query/api/queries/QueryHandler.java
@@ -0,0 +1,12 @@
+package com.techbank.account.query.api.queries;
+
+import com.techbank.cqrs.core.domain.BaseEntity;
+
+import java.util.List;
+
+public interface QueryHandler {
+    List<BaseEntity> handle(FindAllAccountsQuery query);
+    List<BaseEntity> handle(FindAccountsByIdQuery query);
+    List<BaseEntity> handle(FindAccountByHolderQuery query);
+    List<BaseEntity> handle(FindAccountWithBalanceQuery query);
+}
